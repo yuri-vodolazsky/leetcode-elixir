57681799c93442e20fd384ff2260d2598ae3f06a
Yurii Vodolazkyi <vodolazky@gmail.com>
2025-01-25 12:15:51 -0700

    Create mix app

---
+++ lib/leetcode_elixir.ex
@@ -0,0 +1,18 @@
+defmodule LeetcodeElixir do
+  @moduledoc """
+  Documentation for `LeetcodeElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LeetcodeElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
