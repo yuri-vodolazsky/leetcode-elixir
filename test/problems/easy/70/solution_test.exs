4dae4570de8820a631c4c5adf62c0751d6674134
Yuri Vod <yurii@vianet.us>
2025-11-09 20:38:03 -0500

    Add memoization to problem 70

--- test/problems/easy/70/solution_test.exs
+++ test/problems/easy/70/solution_test.exs
@@ -8,9 +8,20 @@ defmodule Leetcode.Elixir.P70.SolutionTest do
     assert Solution.climb_stairs(3) == 3
     assert Solution.climb_stairs(4) == 5
     assert Solution.climb_stairs(5) == 8
-    assert Solution.climb_stairs(6) == 12
+    assert Solution.climb_stairs(6) == 13
+    assert Solution.climb_stairs(7) == 21
+    assert Solution.climb_stairs(8) == 34
+    Enum.each(1..100, & Solution.climb_stairs(&1))
   end
 end
+
+# 1 1
+# 2
+
+
+# 1 1 1
+# 2 1
+# 1 2
 
 # 1 1 1 1
 # 2 1 1
@@ -34,8 +45,9 @@ end
 # 1 1 1 2 1
 # 1 1 1 1 2
 # 2 2 1 1
+# 1 2 2 1
+# 1 1 2 2
 # 2 1 2 1
 # 2 1 1 2
 # 1 2 1 2
-# 1 1 2 2
 # 2 2 2
